08d9ec337b4842b4a973be4f58f3462810f73fa8
Andy Situ <and.situ@gmail.com>
2015-08-17 18:19:00 -0700

    board IP to have draw & update function

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-var board = (function() {
-	var canvas = document.getElementById("canvas"),
-		ctx = canvas.getContext('2d'),
+var board,
+	canvas;
+
+canvas = document.getElementById("canvas")
+
+board = function() {
+	var ctx = canvas.getContext('2d'),
 		x = 0,
 		y = 0,
-		boardWidth = 20,
-		boardHeight = 10,
+		speed = 5,
+		boardWidth = 40,
+		boardHeight = 15,
 		width = canvas.width,
 		height = canvas.height;
 
-	function makeBoard() {
+	function draw() {
 		var yPoint = height - boardHeight;
 		ctx.beginPath();
 		ctx.fillRect(x, yPoint, boardWidth, boardHeight);
+	}
+
+	function update(elapsed) {
 
 	}
 
-	makeBoard();
+	return {
+		darw: draw,
+		update: update;
+	}
 
-})()
+});
+
+function init() {
+
+}
