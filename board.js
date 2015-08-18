a4ff0fa15518c6bbbba52475cd0aa1de86c8373b
Andy Situ <and.situ@gmail.com>
2015-08-18 13:31:13 -0700

    moved values from board into controller obj values

--- board.js
+++ board.js
@@ -1,24 +1,15 @@
 board = (function() {
-	var	x = 0,
-		y = 0,
-		speed = 20,
-		boardWidth = 40,
-		boardHeight = 15,
-		width = canvas.width,
-		height = canvas.height,
-		yPoint = height - boardHeight,
-		maxX = width - boardWidth;
 
-	function draw(ctx) {
-		ctx.fillRect(x, yPoint, boardWidth, boardHeight);
+	function draw(obj, ctx) {
+		ctx.fillRect(obj.x, obj.yPoint, obj.boardWidth, obj.boardHeight);
 	}
 
-	function update(elapsed, moveDir) { // 1 for right, -1 for left
-		x += moveDir * speed;
-		if (x < 0) {
-			x = 0;
-		} else if (x > maxX) {
-			x = maxX;
+	function update(obj, elapsed, moveDir) { // 1 for right, -1 for left
+		obj.x += moveDir * obj.speed;
+		if (obj.x < 0) {
+			obj.x = 0;
+		} else if (obj.x > obj.maxX) {
+			obj.x = obj.maxX;
 		}
 	}
 
--- controller.js
+++ controller.js
@@ -3,25 +3,40 @@ controller = (function(){
 	width = canvas.width,
 	height = canvas.height;
 
+	var values = {
+		x: 0,
+		y: 0,
+		speed: 20,
+		boardWidth: 60,
+		boardHeight: 15,
+		width: canvas.width,
+		height: canvas.height
+	};
+
+	values["yPoint"] = values.height - values.boardHeight;
+	values["maxX"] = values.width - values.boardWidth;
+
 	function clearBoard(ctx) {
 		ctx.clearRect(0, 0, width, height);
 	}
 
 	function update(elapsed) {
-		board.update(elapsed, moveValue);
+		board.update(values, elapsed, moveValue);
 		moveValue = 0;
 		ball.update(elapsed);
 	}
 
 	function draw(ctx) {
 		clearBoard(ctx);
-		board.draw(ctx);
 		ball.draw(ctx);
+		board.draw(values, ctx);
 	}
 
 	function movement(value){
 		moveValue = value;
 	}
+
+	console.log(values);
 
 	return {
 		draw: draw,
