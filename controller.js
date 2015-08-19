1641d788f522adbd46d297abb98f6d3228d10556
Andy Situ <and.situ@gmail.com>
2015-08-18 17:35:41 -0700

    Changed values obj to v (name)

--- controller.js
+++ controller.js
@@ -3,9 +3,8 @@ controller = (function(){
 	width = canvas.width,
 	height = canvas.height;
 
-	var values = {
+	var v = {
 		x: 0,
-		y: 0,
 		speed: 20,
 		boardWidth: 60,
 		boardHeight: 15,
@@ -15,27 +14,27 @@ controller = (function(){
 		yBall: 20,
 		vx: 5,
 		vy: 2,
-		radius: 15,
+		radius: 10,
 		fullPI: 2* Math.PI
 	};
 
-	values["yPoint"] = values.height - values.boardHeight;
-	values["maxX"] = values.width - values.boardWidth;
+	v["yPoint"] = v.height - v.boardHeight;
+	v["maxX"] = v.width - v.boardWidth;
 
 	function clearBoard(ctx) {
 		ctx.clearRect(0, 0, width, height);
 	}
 
 	function update(elapsed) {
-		board.update(values, elapsed, moveValue);
+		board.update(v, elapsed, moveValue);
 		moveValue = 0;
-		ball.update(values, elapsed);
+		ball.update(v, elapsed);
 	}
 
 	function draw(ctx) {
 		clearBoard(ctx);
-		ball.draw(values, ctx);
-		board.draw(values, ctx);
+		ball.draw(v, ctx);
+		board.draw(v, ctx);
 	}
 
 	function movement(value){
