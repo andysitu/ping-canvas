07058304b32d9d28674220fcf28b07cb28cac0da
Andy Situ <and.situ@gmail.com>
2015-08-18 17:54:10 -0700

    increase board hitbox to include radius/2

--- hit.js
+++ hit.js
@@ -3,8 +3,8 @@ hit = (function() {
 	function hitBoard(x, width, top, ballX, ballY, radius) { // decides where the ball hit the board
 		var xEnd = x + width;
 
-		if (ballY > top) {
-			if (ballX > x && ballX < xEnd) {
+		if (ballY > top - radius/2) {
+			if (ballX > x - radius/2 && ballX < xEnd+ radius/2) {
 				if (ballY < top + radius) {
 					return true;
 				} else {
